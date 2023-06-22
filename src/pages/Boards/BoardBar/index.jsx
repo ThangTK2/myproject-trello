19db90677680d27e67e6e4d4e9666831cae5d73a
ThangTK2 <nht1072@gmail.com>
2023-06-22 16:46:54 +0700

    cautrucpj

---
+++ src/pages/Boards/BoardBar/index.jsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+
+function BoarBar() {
+  return (
+    <Box sx={{ backgroundColor: 'primary.dark', width: '100%', height: (theme) => theme.trello.boardBarHeight, display: 'flex', alignItems: 'center' }}>
+      Board Bar
+    </Box>
+  )
+}
+
+export default BoarBar
